d41ec9fb58db3c6d27d522060326843d3822141f
TheoGaerel <70510651+TheoGaerel@users.noreply.github.com>
2023-02-04 20:11:43 +0100

    Canvas Enemy

--- Assets/Scripts/Sc_Enemy.cs
+++ Assets/Scripts/Sc_Enemy.cs
@@ -12,19 +12,31 @@ public abstract class Sc_Enemy : Sc_Entity
 
     private bool b_ready = false;
     protected NavMeshAgent navMeshAgent;
-
+    protected Sc_CanvasEnemy canvasEnemy;
     protected virtual void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         f_attackdelay = F_MAX_ATTACK_DELAY;
         f_attackdelay += Random.Range(-1f, 1f);
-
+        canvasEnemy = GetComponentInChildren<Sc_CanvasEnemy>();
+        for (int i = 0; i < canvasEnemy.list_hearts.Count; i++)
+        {
+            if (i < f_lifeAmount) canvasEnemy.list_hearts[i].gameObject.SetActive(true);
+            else canvasEnemy.list_hearts[i].gameObject.SetActive(false);
+        }
         StartCoroutine(RoutineDelayStart());
     }
     public override void OnDamage()
     {
         if (!b_ready) return;
         f_lifeAmount--;
+
+        for (int i = 0; i < canvasEnemy.list_hearts.Count; i++)
+        {
+            if (i < f_lifeAmount) canvasEnemy.list_hearts[i].gameObject.SetActive(true);
+            else canvasEnemy.list_hearts[i].gameObject.SetActive(false);
+        }
+
         if (f_lifeAmount <= 0)
         {
             this.gameObject.SetActive(false);
